6b7373203c5785045bf96293116c48bc61348c96
Kailiak Evgeniy <kailiakeugene@mail.ru>
2020-09-08 00:14:37 +0300

    add pdf-maker

--- download_studybook.py
+++ download_studybook.py
@@ -26,16 +26,19 @@ def parse_html(html):
 pdf = FPDF()
 url = 'https://reader.lecta.rosuchebnik.ru/8233-63/data/page-{}.xhtml'
 image_url = 'https://reader.lecta.rosuchebnik.ru/8233-63/data/'
-for i in range(1, 2):
+temp_image = "temp.png"
+
+for i in range(1, 370):
     current_url = url.format(make_inset(i))
     
     html = download_from_the_internet(current_url)
     image_url = urljoin(image_url, parse_html(html))
     
     response = requests.get(image_url, stream=True)
-    image = response.content
-    print(image)
-    #pdf.add_page()
-    #pdf.image(image,x,y,w,h)
+    image_data = response.content
+    with open(temp_image, "wb") as f:
+        f.write(image_data)
+    pdf.add_page()
+    pdf.image(temp_image)
     sleep(0.1)
 pdf.output("algebra8.pdf", "F")
